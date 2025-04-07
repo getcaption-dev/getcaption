7389e14977a2cf8c42508b39323bc47543023264
getcaption-dev <getcaption.service@gmail.com>
2025-04-07 23:31:24 +0900

    Add next.config.js and fix environment variable issue

---
+++ next.config.js
@@ -0,0 +1,14 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+      GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+      GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+      NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    },
+  };
+  
+  module.exports = nextConfig;
+  
