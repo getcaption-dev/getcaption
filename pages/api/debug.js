9514d6de004c60d9f4969e9afeafd9702280a22c
getcaption-dev <getcaption.service@gmail.com>
2025-04-07 23:50:32 +0900

    Remove exposed debug endpoint

--- pages/api/debug.js
+++
@@ -1,8 +0,0 @@
-// pages/api/debug.js
-export default function handler(req, res) {
-  res.status(200).json({
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    secret: process.env.GOOGLE_CLIENT_SECRET,
-    nextauthUrl: process.env.NEXTAUTH_URL,
-  });
-}
